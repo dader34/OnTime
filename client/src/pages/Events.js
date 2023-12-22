3b09010968a760e5bef818d94f323733ff6ea483
dader34 <56900960+dader34@users.noreply.github.com>
2023-12-21 17:54:14 -0800

    Styles

--- client/src/pages/Events.js
+++ client/src/pages/Events.js
@@ -20,7 +20,7 @@ const Events = () => {
         <div className="container mt-5" style={{ display: 'flex', justifyContent: 'space-evenly', alignItems: 'flex-start' }}>
           <div className="row" style={{justifyContent:'space-around'}}>
             {events.length ? events.map((event) => (
-              <div key={event.id} className="col-md-6 mb-4">
+              <div key={event.id} className={events.length > 1 ? "col-md-6 mb-4" : "col-md-12 d-flex justify-content-center mb-2" }>
                 <EventCard event={event} />
               </div>
             )) : <><h1>Looks like no one has made a post yet, be the first!</h1><br/><Link to="/create" className="rainbow btn btn-primary" style={{width:'12%'}}>
